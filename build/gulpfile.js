399b551397ad6e1e50d320ff84819a66de1f5bfc
peerhenry <peerhenry@gmail.com>
2016-06-19 18:16:52 +0200

    made gulp file

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -4,8 +4,47 @@ var gulp = require('gulp'),
     less = require('gulp-less'),
     pug = require('gulp-pug'),
     runSequence = require('run-sequence');
-    
+
+gulp.task('pug', function(){
+    return gulp.src(['../src/public/**/*.pug'])
+    .pipe(pug({
+        // pug options here
+    }))
+    .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('less', function(){
+    return gulp.src(['../src/public/**/*.less'])
+    .pipe(less({
+        paths: [path.join(__dirname, 'less', 'includes')]
+    }))
+    .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('ts', function(){
+    return gulp.src(['../src/public/**/*.less'])
+    .pipe(tsc({
+        module: 'amd',
+        target: 'ES5'
+    }))
+    .on('error', uril.log)
+    .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('compile-client', ['pug', 'less', 'ts']);
+
+gulp.task('compile-server', function(){
+    return gulp.src(['../src/**/*.ts', '!../src/typings/**', '!../src/public/**'])
+    .pipe(tsc({
+        module: 'commonjs',
+        target: 'ES5'
+    }))
+    .on('error', util.log)
+    .pipe(gulp.dest('./'));
+});
 
 gulp.task('compile', ['compile-server', 'compile-client']);
 
-gulp.task('compile-client', ['pug', 'less', 'ts']);
+gulp.task('default', function(){
+    runSequence('compile', 'watch');
+});
