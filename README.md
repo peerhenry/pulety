3d1d91dd2b90800764c6887293a2d85d2e30c6e2
peerhenry <peerhenry@gmail.com>
2016-06-26 22:21:14 +0200

    expanded gulp with livereload, cache, autoprefixer and cssmin

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pulety
-nodejs with pug, less and typescript
+Nodejs with pug, less and typescript.
+
+The project uses gulp as task runner. All *.pug, *.less and *.ts files are compiled from the src folder to the build folder. This way, the server will not need to perform view compilation. 
+
+All the usual commands for a node project are run from the build folder, with one exception:
+Type definitions must installed in the src folder, so commands like 
+`typings install dt~<your-definition> --save`
+must be run from within the src folder.
+Git commands should be executed from the root folder.
+
+Running gulp will compile the source code, run the server, watch for changes and enable livereload.
--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,42 +1,73 @@
-var gulp = require('gulp'),
-    util = require('gulp-util'),
-    tsc = require('gulp-typescript'),
-    less = require('gulp-less'),
-    pug = require('gulp-pug'),
-    runSequence = require('run-sequence');
-    nodemon = require('gulp-nodemon');
+var gulp = require('gulp');
+var util = require('gulp-util');
+var tsc = require('gulp-typescript');
+var less = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
+var pug = require('gulp-pug');
+var runSequence = require('run-sequence');
+var nodemon = require('gulp-nodemon');
+var cache = require('gulp-cached');
+var livereload = require('gulp-livereload');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+
+var serverTypescript = ['../src/**/*.ts', '!../src/typings/**', '!../src/public/**', '!../src/**/*.d.ts'];
 
 gulp.task('pug', function(){
     return gulp.src(['../src/public/**/*.pug'])
+    .pipe(cache('pugCache'))
     .pipe(pug({
         // pug options here
     }))
-    .pipe(gulp.dest('./public/'));
+    .pipe(gulp.dest('./public/'))
+    .pipe(livereload());
 });
 
 gulp.task('less', function(){
     return gulp.src(['../src/public/**/*.less'])
+    .pipe(cache('lessCache'))
     .pipe(less({
         //paths: [path.join(__dirname, 'less', 'includes')]
     }))
-    .pipe(gulp.dest('./public/'));
+    .pipe(autoprefixer({
+        browsers: [
+            '> 1%',
+            'last 2 versions',
+            'firefox >= 4',
+            'safari 7',
+            'safari 8',
+            'IE 8',
+            'IE 9',
+            'IE 10',
+            'IE 11'
+        ],
+        cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./public/'))
+    .pipe(livereload());
 });
 
 gulp.task('ts', function(){
     return gulp.src(['../src/public/**/*.ts'])
+    .pipe(cache('tsCache'))
     .pipe(tsc({
         module: 'amd',
         target: 'ES5',
         removeComments: true
     }))
     .on('error', util.log)
-    .pipe(gulp.dest('./public/'));
+    .pipe(gulp.dest('./public/'))
+    .pipe(livereload());
 });
 
 gulp.task('compile-client', ['pug', 'less', 'ts']);
 
 gulp.task('compile-server', function(){
-    return gulp.src(['../src/**/*.ts', '!../src/typings/**', '!../src/public/**', '!../src/**/*.d.ts'])
+    return gulp.src(serverTypescript)
+    .pipe(cache('serverCache'))
     .pipe(tsc({
         module: 'commonjs',
         target: 'ES5',
@@ -53,9 +84,31 @@ gulp.task('nodemon', function(){
         script: 'server.js'
         , ext: 'html css js'
         , env: { 'NODE_ENV': 'development' }
+        , ignore: 'public/**'
+    })
+    .on('restart', function(){
+        gulp.src('server.js')
+        .pipe(livereload())
+        .pipe(notify('Reloading page, please wait...'));
     });
 });
 
+gulp.task('livereload-listen', function(){
+    livereload.listen();
+});
+
+gulp.task('watch-server', function(){
+    gulp.watch(serverTypescript, ['compile-server']);
+});
+
+gulp.task('watch-client', function(){
+    gulp.watch('../src/public/**/*.pug', ['pug']);
+    gulp.watch('../src/public/**/*.less', ['less']);
+    gulp.watch('../src/public/**/*.ts', ['ts']);
+});
+
+gulp.task('watch', ['watch-server', 'watch-client']);
+
 gulp.task('default', function(){
-    runSequence('compile', 'nodemon');
+    runSequence('compile', 'livereload-listen', 'nodemon', 'watch');
 });
